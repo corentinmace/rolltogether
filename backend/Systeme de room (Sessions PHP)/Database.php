f2cd6aea6aa966f958eea2add0c6b1ddc85e6170
corenitnmace <corentin.mace@ynov.com>
2019-12-20 16:32:01 +0100

    Ajout partie BDD

---
+++ backend/Systeme de room (Sessions PHP)/Database.php
@@ -0,0 +1,74 @@
+<?php
+
+class Database{
+    private static $_instance = null; // L'attribut d'instance du Singleton
+
+    private $ini = null; // La variable qui va stocker l'objet .ini
+    private $db = null; // La variable qui va stocker l'objet PDO
+
+    /**
+     * Database constructor.
+     */
+    private function __construct(){}
+
+    public function __destruct(){
+        self::$_instance = null;
+    }
+
+    /**
+     * @return Database|null Si la classe a déjà été instanciée, on la renvoie. Sinon, on la crée
+     */
+    public static function getInstance(){
+        if(is_null(self::$_instance)){ // Si aucune instance n'a été créée
+            self::$_instance = new Database(); // On en crée une
+        }
+        return self::$_instance; // On renvoie l'instance créée
+    }
+
+    /**
+     * @param $ini // On stocke dans l'objet le chemin vers le fichier de conf ini
+     */
+    public function setIni($ini){
+        if(!is_null($ini) && "" != $ini){ // Si le chemin est valide
+            $this->ini = $ini; // On enregistre le fichier ini
+            $this->connectDatabase(); // On se connecte à la bdd
+        }
+    }
+
+    /**
+     * @return array|false On retourne le fichier parsé
+     */
+    private function getIniData(){
+        if(parse_ini_file($this->ini) == false){ // Si l'ouverture du fichier échoue
+            throw new InvalidArgumentException("Chemin de fichier incorrect");
+        }else{
+            return parse_ini_file($this->ini); // On renvoie les valeurs du fichier
+        }
+    }
+
+    /**
+     * Fonction de connexion à la database avec gestion des erreurs
+     */
+    private function connectDatabase(){
+        try{
+            $this->getIniData();  // On essaye d'ouvrir le fichier ini
+        }catch(InvalidArgumentException $i){ // S'il y a une erreur
+            echo "Erreur lors de l'ouverture du fichier de configuration ini: " . $i->getMessage();
+            exit(-1);
+        }
+
+        try{
+            $this->db = new PDO($this->getIniData()['dbname'], $this->getIniData()['user'], $this->getIniData()['password']); // On se connecte à la database
+        }catch(PDOException $e){
+            echo "Erreur lors de la connexion à la BDD: " . $e->getMessage(); // On affiche l'erreur
+        }
+    }
+
+    /**
+     * @param $q // La requête à effectuer
+     * @return mixed // Renvoie le résultat de la requête
+     */
+    public function query($query){
+        return $this->db->query($query);
+    }
+}
--- backend/Systeme de room (Sessions PHP)/index.php
+++ backend/Systeme de room (Sessions PHP)/index.php
@@ -1,5 +1,7 @@
 <?php
 
+  require "Database.php";
+
 function generateRandomString($length = 25) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
@@ -22,10 +24,51 @@ function createSession() {
   echo '<a href=index.php?id='.session_id().'>Inviter dans la partie</a>';
 
   echo '<p>Salle : '.session_id().'</p>';
+
+  $id = session_id();
+
+if(isset($_POST['nbJoueur'])) {
+  $nbJoueur=$_POST['nbJoueur'];
+} else {
+  $nbJoueur=0;
+}
+
+if(isset($_POST['visible'])) {
+  $visible=false;
+} else {
+  $visible=true;
+}
+
+if(isset($_POST['nom'])) {
+  $nomGame=$_POST['nom'];
+} else {
+  $nomGame="";
+}
+
+if(isset($_POST['Id_Scenario'])) {
+  $idScenario=$_POST['Id_Scenario'];
+} else {
+  $idScenario=1;
+}
+
+if(isset($_POST['Id_User'])) {
+  $idUser=$_POST['Id_User'];
+} else {
+  $idUser=1;
+}
+
+  $connect = Database::getInstance();
+
+  $connect->setIni("db.ini");
+  // $partie = $connect->query("SELECT * FROM partie;")->fetch(PDO::FETCH_ASSOC);
+  $connect->query("INSERT INTO partie VALUES(0, $nbJoueur, $visible, '$nomGame', $idScenario, $idUser, '$id')");
+
+
 }
 
 if (isset($_POST['button'])) {
   createSession();
+
 }
 
 if (isset($_GET['id'])) {
