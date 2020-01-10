c8d4ccb1f7eb20eb512a1f11018e2cb151911aee
corenitnmace <corentin.mace@ynov.com>
2020-01-10 14:13:48 +0100

    Inscription ok

--- backend/inscription.php
+++ backend/inscription.php
@@ -7,24 +7,36 @@ if(isset($_POST) && !empty($_POST)){ // Si le formulaire a été rempli
         $pseudo = htmlspecialchars($_POST['pseudo']); // On récupère le pseudo
         $email = htmlspecialchars($_POST['email']); // On récupère l'email
         $mdp = htmlspecialchars($_POST['mdp']); // On récupère le mdp
+        $hashedMdp = hash("SHA512", $mdp);
 
         $db_infos = parse_ini_file("db.ini"); // On récupère les infos de connexion depuis le fichier ini
         $db = new PDO($db_infos['dsn'], $db_infos['username'], $db_infos['password']); // On se connecte à la database
 
-        $query = $db->prepare("INSERT INTO User(pseudo, password, email, nom, prenom) VALUES(:pseudo, :password, :email, :nom, :prenom);"); // On prépare la requête
-
+        $query = $db->prepare("INSERT INTO user(Pseudo, Password, Email, Nom, Prenom) VALUES(:pseudo, :password, :email, :nom, :prenom);"); // On prépare la requête
         /* On insère les variables dans la requête */
         $query->bindParam(":pseudo", $pseudo);
-        $query->bindParam(":password", hash("SHA512", $mdp)); // On hash le mot de passe pour ne pas le stocker en clair dans la database
+        $query->bindParam(":password", $hashedMdp); // On hash le mot de passe pour ne pas le stocker en clair dans la database
         $query->bindParam(":nom", $nom);
         $query->bindParam(":prenom", $prenom);
         $query->bindParam(":email", $email);
-        try{
-            $db->exec($query); // On exécute la requête
-        }catch (Error $error){ // S'il y a une erreur
-            echo "Erreur: " . $error; // On l'affiche
+        if ((preg_match (" /^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/ " , $email)) && (preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}$#', $mdp)))
+        {
+          try{
+              $query->execute(); // On exécute la requête
+              header("Location: ../frontend/page_connexion.html"); // On redirige vers la page de connexion
+              // $query->exec();
+              // die(var_dump($query));
+              // die (var_dump($query));
+            }catch (Error $error){ // S'il y a une erreur
+                echo "Erreur: " . $error; // On l'affiche
+            }
+        } else {
+          if (!preg_match (" /^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/ " , $email)) {
+            echo 'email non conforme';
+          } else if (!preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}$#', $mdp)) {
+            echo 'mot de passe non conforme';
+          }
         }
-        header("Location: ../frontend/page_connexion.html"); // On redirige vers la page de connexion
 
         $db = null; // On ferme la connexion
     }
