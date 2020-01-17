5220a2031ed3097055d966e89cd5ca5755e1a663
Eschyle <francois.caillat@viacesi.fr>
2020-01-17 17:26:55 +0100

    ADD dropdown menu for user part

--- frontend/header.php
+++ frontend/header.php
@@ -1,9 +1,21 @@
-    <script src="../node_modules/@fortawesome/fontawesome-free/js/all.js"></script>
+<script src="../node_modules/@fortawesome/fontawesome-free/js/all.js"></script>
 
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 <header>
     <div id="logo">
         <a href="index.php"><img src="assets/img/logo_blanc.png" alt="Accueil"></a>
-        <a id="user" href="user.php" style="color: white"><i class="fas fa-user fa-2x"></i></a>
+
+        <div class="dropdown">
+            <a class="" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               <i class="fas fa-user fa-2x"></i>
+            </a>
+            <div class="dropdown-menu" id="dropdownUserMenu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="page_inscription.php">Inscription</a>
+                <a class="dropdown-item" href="page_connexion.php">Connexion</a>
+            </div>
+        </div>
     </div>
 </header>
