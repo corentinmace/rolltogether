d09112d2eed107bd2a6af3cb75feb7b546fc4fdc
Eschyle <francois.caillat@viacesi.fr>
2020-01-17 13:08:42 +0100

    REFACTO footer

---
+++ frontend/footer.php
@@ -0,0 +1,20 @@
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui eaque quidem est sed totam praesentium voluptates odit, beatae officia minus molestias porro aperiam ea repellat fuga cum eligendi laudantium? Numquam.
+            </div>
+            <div class="col-4">
+                <div id="ico">
+                    <a href="https://www.facebook.com/roll2gether/"><img src="https://img.icons8.com/carbon-copy/100/000000/facebook-new.png"></a>
+                    <a href="https://twitter.com/together_roll"><img src="https://img.icons8.com/carbon-copy/100/000000/twitter.png"></a>
+                    <a href="https://www.instagram.com/roll2gether/?hl=fr"><img src="https://img.icons8.com/carbon-copy/100/000000/instagram-new.png"></a>
+                </div>
+            </div>
+            <div class="col-4">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti ullam eveniet, quia accusamus et quo illum dolore recusandae deserunt laborum omnis officiis ab reprehenderit modi esse tempore optio aliquam maxime.
+            </div>
+        </div>
+    </div>
+</footer>
--- frontend/page_inscription.php
+++ frontend/page_inscription.php
@@ -43,25 +43,9 @@
     </div>
 </body>
 
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-4">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui eaque quidem est sed totam praesentium voluptates odit, beatae officia minus molestias porro aperiam ea repellat fuga cum eligendi laudantium? Numquam.
-            </div>
-            <div class="col-4">
-                <div id="ico">
-                    <a href="https://www.facebook.com/roll2gether/"><img src="https://img.icons8.com/carbon-copy/100/000000/facebook-new.png"></a>
-                    <a href="https://twitter.com/together_roll"><img src="https://img.icons8.com/carbon-copy/100/000000/twitter.png"></a>
-                    <a href="https://www.instagram.com/roll2gether/?hl=fr"><img src="https://img.icons8.com/carbon-copy/100/000000/instagram-new.png"></a>
-                </div>
-            </div>
-            <div class="col-4">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti ullam eveniet, quia accusamus et quo illum dolore recusandae deserunt laborum omnis officiis ab reprehenderit modi esse tempore optio aliquam maxime.
-            </div>
-        </div>
-    </div>
-</footer>
+<?php
+    include("footer.php");
+?>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
