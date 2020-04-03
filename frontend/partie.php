73524bb8a3ee8c6eb6a787c95dc9d4ef1307e7c2
Quentin Bensmaine <quentin.bensmaine@ynov.com>
2020-04-03 18:14:21 +0200

    update-game2.php

--- frontend/partie.php
+++ frontend/partie.php
@@ -14,10 +14,10 @@
 
 <body>
     <header>
-    <div class="header-banner">
-      <a href="index.php"><img src="assets/img/logo.png" alt="Accueil"></a>
-      <a href="user.php"><img id="user" src="https://img.icons8.com/ios-glyphs/64/000000/user--v1.png"></a>
-    </div>
+        <div class="header-banner">
+            <a href="index.php"><img src="assets/img/logo.png" alt="Accueil"></a>
+            <a href="user.php"><img id="user" src="https://img.icons8.com/ios-glyphs/64/000000/user--v1.png"></a>
+        </div>
         <nav id="main-menu">
             <ul class="nav-bar">
                 <li class="nav-button-game"><a href="index.php">ACCUEIL</a></li>
@@ -29,32 +29,42 @@
         </nav>
     </header>
     <main>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia in expedita distinctio? Illum vero, provident sequi laboriosam amet voluptatibus numquam consectetur, architecto voluptatum ex neque esse eaque perspiciatis optio velit.
+        <div id="contenu">
+            <div id="contenu__text" class="content">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui eaque quidem est sed totam praesentium voluptates odit, beatae officia minus molestias porro aperiam ea repellat fuga cum eligendi laudantium? Numquam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui eaque quidem est sed totam praesentium voluptates odit, beatae officia minus molestias porro aperiam ea repellat fuga cum eligendi laudantium? Numquam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui eaque quidem est sed totam praesentium voluptates odit, beatae officia minus molestias porro aperiam ea repellat fuga cum eligendi laudantium? Numquam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui eaque quidem est sed totam praesentium voluptates odit, beatae officia minus molestias porro aperiam ea repellat fuga cum eligendi laudantium? Numquam.</p>
+            </div>
+            <div id="contenu__diapos">
+                <div id="contenu__diapos__mini-diapos" class="content">Lorem ipsum dolor</div>
+                <div id="contenu__diapos__selected" class="content">Lorem ipsum dolor</div>
+            </div>
+            <div id="contenu__chat" class="content">Lorem ipsum dolor</div>
+            <div id="contenu__music" class="content">Lorem ipsum dolor</div>
         </div>
         <div id="ligne">
         </div>
     </main>
     <footer>
-          <div class="container">
-              <div class="row">
-                  <div class="col-4">
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui eaque quidem est sed totam praesentium voluptates odit, beatae officia minus molestias porro aperiam ea repellat fuga cum eligendi laudantium? Numquam.
-                  </div>
-                  <div class="col-4">
-                      <div id="ico">
-                          <a href="https://www.facebook.com/roll2gether/"><img src="https://img.icons8.com/carbon-copy/100/000000/facebook-new.png"></a>
-                          <a href="https://twitter.com/together_roll"><img src="https://img.icons8.com/carbon-copy/100/000000/twitter.png"></a>
-                          <a href="https://www.instagram.com/roll2gether/?hl=fr"><img src="https://img.icons8.com/carbon-copy/100/000000/instagram-new.png"></a>
-                      </div>
-                  </div>
-                  <div class="col-4">
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti ullam eveniet, quia accusamus et quo illum dolore recusandae deserunt laborum omnis officiis ab reprehenderit modi esse tempore optio aliquam maxime.
-                  </div>
-              </div>
-          </div>
-      </footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui eaque quidem est sed totam praesentium voluptates odit, beatae officia minus molestias porro aperiam ea repellat fuga cum eligendi laudantium? Numquam.
+                </div>
+                <div class="col-4">
+                    <div id="ico">
+                        <a href="https://www.facebook.com/roll2gether/"><img src="https://img.icons8.com/carbon-copy/100/000000/facebook-new.png"></a>
+                        <a href="https://twitter.com/together_roll"><img src="https://img.icons8.com/carbon-copy/100/000000/twitter.png"></a>
+                        <a href="https://www.instagram.com/roll2gether/?hl=fr"><img src="https://img.icons8.com/carbon-copy/100/000000/instagram-new.png"></a>
+                    </div>
+                </div>
+                <div class="col-4">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti ullam eveniet, quia accusamus et quo illum dolore recusandae deserunt laborum omnis officiis ab reprehenderit modi esse tempore optio aliquam maxime.
+                </div>
+            </div>
+        </div>
+    </footer>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
