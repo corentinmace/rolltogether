7bdd774ee75605e7314c987bb9cbede99aa8cbcb
Francois <f.caillat@autosecuritas.fr>
2020-04-10 17:20:04 +0200

    UPDATE espacement nav

--- frontend/nav.php
+++ frontend/nav.php
@@ -1,16 +1,16 @@
 <script src="assets/fontawesome-free/js/all.js"></script>
 <div id="navigation-r2">
     <ul class="nav">
-      <li class="nav-item">
+      <li class="nav-item col">
         <a class="nav-link active nav-r2" href="game.php">JOUER</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item col">
         <a class="nav-link nav-r2" href="biblio.php">BIBLIOTHEQUE</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item col">
         <a class="nav-link nav-r2" href="shop.php">BOUTIQUE</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item col">
         <a class="nav-link nav-r2" href="user.php">ESPACE UTILISATEUR</a>
       </li>
     </ul>
