52f481ef0b8175e2b3dc2ad6411e015addbe557b
Francois <f.caillat@autosecuritas.fr>
2020-04-10 17:18:17 +0200

    CORRECT header

--- frontend/nav.php
+++ frontend/nav.php
@@ -1,17 +1,17 @@
 <script src="assets/fontawesome-free/js/all.js"></script>
-<div id="navigation-r2" class="col-sm-12 col-md-12 col-slg-12">
+<div id="navigation-r2">
     <ul class="nav">
-        <li class="nav-item col">
-            <a class="nav-link active nav-r2" href="{{ path('game_route') }}">JOUER</a>
-        </li>
-        <li class="nav-item  col">
-            <a class="nav-link nav-r2" href="{{ path('biblio_route') }}">BIBLIOTHEQUE</a>
-        </li>
-        <li class="nav-item col">
-            <a class="nav-link nav-r2" href="{{ path('shop_route') }}">BOUTIQUE</a>
-        </li>
-        <li class="nav-item col">
-            <a class="nav-link nav-r2" href="{{ path('profile_route') }}">ESPACE UTILISATEUR</a>
-        </li>
+      <li class="nav-item">
+        <a class="nav-link active nav-r2" href="game.php">JOUER</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link nav-r2" href="biblio.php">BIBLIOTHEQUE</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link nav-r2" href="shop.php">BOUTIQUE</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link nav-r2" href="user.php">ESPACE UTILISATEUR</a>
+      </li>
     </ul>
 </div>
